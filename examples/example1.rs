9f9617fdd1bf3e9bab46e041e9e0cd7d4af30bef
Javagedes <joey.vagedes@gmail.com>
2020-11-01 19:34:42 -0600

    Have separate control of the servers stdin and stdout (and separate from mcwrs i/o). Can get line by line the output of the mc server

---
+++ examples/example1.rs
@@ -0,0 +1,8 @@
+
+use mcw_rs::*;
+
+fn main() {
+    let server = McServer::init();
+
+    server.test();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,53 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//use tokio::process::*;
+use std::process::Stdio;
+use std::io::BufReader;
+use std::process::*;
+use std::io::BufRead;
+
+//use tokio::io::{self, BufRead,AsyncBufReadExt, BufReader, AsyncReadExt, Lines};
+//use tokio::prelude::*;
+
+pub struct McServer {
+
+    child: Child,
+    stdin: ChildStdin,
+    stdout: BufReader<ChildStdout>
+
+}
+
+impl McServer {
+
+    pub fn init()->McServer {
+        let mut child = Command::new("java")
+                .current_dir("./server")
+                .arg("-Xmx1024M")
+                .arg("-Xms1024M")
+                .arg("-jar")
+                .arg("server.jar")
+                .arg("nogui")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()
+                .expect("Failed to spawn child process");
+  
+        let stdin = child.stdin.take().unwrap();
+
+        let stdout = BufReader::new(child.stdout.take().unwrap());
+
+        return McServer {
+            child,
+            stdin,
+            stdout
+        }
+    }
+
+    pub fn test(self) {
+       
+        let lines = self.stdout.lines();
+
+        lines.for_each(|line| {
+            println!("{:?}", line);
+        })
     }
 }
